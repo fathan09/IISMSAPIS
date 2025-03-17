4861037d55a8d037a79d5f735daed595ae3724f9
fathan09 <totobagindang@gmail.com>
2025-03-17 19:10:53 +0800

    Update ProductEndpoint.cs barcode to byte[]

--- Endpoints/ProductEndpoint.cs
+++ Endpoints/ProductEndpoint.cs
@@ -29,11 +29,12 @@ public static class ProductEndpoint {
             return product is null ? Results.NotFound() : Results.Ok(product.ToProductDetailsDto());
         }).WithName(GetProductEndpointName);
 
-        group.MapPost("/create", async (CreateProductDto newProduct, IISMSContext dbContext) => {
+        group.MapPost("/create", async(CreateProductDto newProduct, IISMSContext dbContext) => {
             string barcodeInfo = $"{newProduct.productName}";
+            
             DateTime timestamp = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
 
-           
+
             var barcodeWriter = new ZXing.SkiaSharp.BarcodeWriter
             {
                 Format = BarcodeFormat.CODE_128,
@@ -47,33 +48,29 @@ public static class ProductEndpoint {
 
             SKBitmap skBitmap = barcodeWriter.Write(barcodeInfo);
 
-     
-            byte[] barcodeBytes;
+        
+            byte[] newBarcode;
             using (var image = skBitmap.Encode(SKEncodedImageFormat.Png, 100))
             using (var ms = new MemoryStream())
             {
                 image.AsStream().CopyTo(ms);
-                barcodeBytes = ms.ToArray();
+                newBarcode = ms.ToArray();
             }
 
-            
-            string barcodeBase64 = Convert.ToBase64String(barcodeBytes);
-
-        
-            Product product = newProduct.ToEntity(barcodeBase64, timestamp);
+            Product product = newProduct.ToEntity(newBarcode, timestamp);
             dbContext.Products.Add(product);
             await dbContext.SaveChangesAsync();
-
-            return Results.CreatedAtRoute(GetProductEndpointName, new { id = product.productId }, product.ToProductDetailsDto());
+            return Results.CreatedAtRoute(GetProductEndpointName, new {id = product.productId}, product.ToProductDetailsDto());
         }).WithParameterValidation();
 
-         group.MapPut("/update/{id}", async (int id, UpdateProductDto updatedProduct, IISMSContext dbContext) => {
+
+        group.MapPut("/update/{id}", async (int id, UpdateProductDto updatedProduct, IISMSContext dbContext) => {
            
             var exisitingProduct = await dbContext.Products.FindAsync(id);
             if(exisitingProduct is null) {
                 return Results.NotFound();
             }
-            string? existingBarcode = exisitingProduct.productBarcode;
+            byte[]? existingBarcode = exisitingProduct.productBarcode;
             DateTime existingTimestamp = exisitingProduct.firstCreationTimestamp;
             dbContext.Entry(exisitingProduct).CurrentValues.SetValues(updatedProduct.ToEntity(id, existingBarcode, existingTimestamp));
             await dbContext.SaveChangesAsync();

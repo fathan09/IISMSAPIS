4691eaed84767dd0084669db4491c0c5b669bb13
fathan09 <totobagindang@gmail.com>
2025-06-28 14:39:59 +0800

    Update OrderDetailsDto.cs

--- Dtos/OrderDetailsDto.cs
+++ Dtos/OrderDetailsDto.cs
@@ -5,6 +5,7 @@ namespace IISMSBackend.Dtos;
 public record class OrderDetailsDto(
     int orderId,
     string customerName,
+    string address,
     DateTime deliveryDate,
     string status,
     ICollection<OrderProduct>? OrderProducts

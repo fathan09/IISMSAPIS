f107664143d2aeb4c36d9b83cfc4d431d5fa4b94
fathan09 <totobagindang@gmail.com>
2025-06-28 20:05:02 +0800

    Update Order.cs

--- Entities/Order.cs
+++ Entities/Order.cs
@@ -8,6 +8,7 @@ public class Order {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int orderId {get; set;}
     public required string customerName { get; set; }
+    public  byte[]? customerSignature {get; set;}
     public required string address { get; set; }
     public required string status { get; set; }
     public required DateTime deliveryDate { get; set; }
